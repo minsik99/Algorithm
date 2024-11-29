9c14307e8da7079b67da5f98999fcddc7ba8cbd6
minsik99 <164865777+minsik99@users.noreply.github.com>
2024-11-29 17:37:41 +0900

    [Silver V] Title: 국회의원 선거, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -2,14 +2,36 @@ import java.io.*;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
+/*
+    [요구 사항]
+
+    입력 : 후보의 수 N, 각각 1번, 2번, 3번 표의 수
+
+    출력 : 1번이 뽑히기 위해 매수해야 할 사람의 수
+
+    [해결 과정]
+
+    우선순위 큐를 통해 최대 힙으로
+    카운트를 줄여가며 구한다
+
+    [슈도 코드]
+
+    입력값 차례대로 저장
+    최대힙에 데이터 삽입
+    카운트 변수 선언
+    최대힙에서 하나씩 팝하면서
+    팝한 값이 1번의 수보다 작을 때까지 카운트 증가
+    이후 카운트 출력
+ */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine()); // 후보의 수
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); // 최대힙
+        int count = 0;
         int vote = Integer.parseInt(br.readLine()); // 1번 후보의 표
-        int count = 0; // 매수해야 하는 사람의 수
 
         // 나머지 후보의 표를 큐에 삽입
         for (int i = 0; i < N - 1; i++) {
@@ -20,13 +42,14 @@ public class Main {
         while (!queue.isEmpty() && queue.peek() >= vote) {
             int maxVotes = queue.poll(); // 가장 많은 표를 가진 후보
             if (maxVotes > 0) {
-                queue.add(maxVotes - 1); // 표를 하나 줄임
-                vote++; // 1번 후보의 표 하나 증가
-                count++; // 매수 횟수 증가
+                queue.add(maxVotes - 1);
+                vote++;
+                count++;
             }
         }
 
-        // 결과 출력
-        System.out.println(count);
+        System.out.print(count);
+
+        br.close();
     }
 }
