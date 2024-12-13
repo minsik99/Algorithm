ad0446d76a6cda85131d072112a20183c22a1b7c
minsik99 <164865777+minsik99@users.noreply.github.com>
2024-12-14 02:13:18 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,51 +1,70 @@
+import java.io.*;
+import java.util.*;
 
+/*
+    [요구 사항]
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+    입력 : 수의 개수 N, N개의 수
+
+    출력 : 좋은 수 출력
+
+    [해결 과정]
+
+    N 개의 수 중 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있으면 좋은 수다
+    정렬 후 투 포인터를 사용해 순서쌍의 개수를 파악한다
+
+    [슈도 코드]
+
+    N 저장
+    배열에 데이터 입력 후 정렬
+    N 만틈 반복하며
+        찾을 값, 시작, 끝 인덱스 선언
+        while(시작 < 끝)
+            좋은 수 찾기
+ */
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int[] arr = new int[N];
 
-		int N = Integer.parseInt(bf.readLine());
-		int result = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
 
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		long[] A = new long[N];
+        for (int i = 0; i < N; i++) {
+            int find = arr[i];
+            int start = 0;
+            int end = N - 1;
 
-		for (int i = 0; i < N; i++) {
-			A[i] = Long.parseLong(st.nextToken());
-		}
-		Arrays.sort(A);
+            while (start < end) {
+                if (start == i) {
+                    start++;
+                    continue;
+                }
+                if (end == i) {
+                    end--;
+                    continue;
+                }
+                int sum = arr[start] + arr[end];
+                if (sum == find) {
+                    count++;
+                    break;
+                } else if (sum < find) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
 
-		for (int k = 0; k < N; k++) {
-			long find = A[k];
-			int i = 0;
-			int j = N - 1;
+        System.out.print(count);
 
-			while (i < j) {
-				if (A[i] + A[j] == find) {
-					if (i != k && j != k) {
-						result++;
-						break;
-					} else if (i == k) {
-						i++;
-					} else if (j == k) {
-						j--;
-					}
-				} else if (A[i] + A[j] < find) {
-					i++;
-				} else {
-					j--;
-				}
-			}
-		}
-		System.out.println(result);
-		bf.close();
-	}
-
+        br.close();
+    }
 }
