f17eac5ca152bb9a3141166ff33b22827b5887e2
minsik99 <164865777+minsik99@users.noreply.github.com>
2025-01-24 19:24:00 +0900

    [Gold V] Title: 집합의 표현, Time: 436 ms, Memory: 52300 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (command == 0) {
+                union(a, b);
+            } else {
+                if (check(a, b)) {
+                    sb.append("YES\n");
+                } else {
+                    sb.append("NO\n");
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    static int find(int a) {
+        if (a == parent[a]) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    static boolean check(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+}
