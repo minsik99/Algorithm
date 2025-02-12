8b0b707882aec4d53414116af9d1a15567c2821f
minsik99 <164865777+minsik99@users.noreply.github.com>
2025-02-12 15:46:51 +0900

    [level 1] Title: 예산, Time: 0.51 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -4,10 +4,9 @@ class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
         Arrays.sort(d);
-        for (int i = 0; i < d.length; i++) {
-            budget -= d[i];
-            if (budget < 0)
-                break;
+        int idx = 0;
+        while (idx < d.length && budget >= d[idx]) { 
+            budget -= d[idx++];
             answer++;
         }
         return answer;
