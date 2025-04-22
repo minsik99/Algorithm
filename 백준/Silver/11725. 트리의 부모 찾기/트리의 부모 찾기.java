2be6be60277b8e4e473d3b90212adf515f229853
minsik99 <164865777+minsik99@users.noreply.github.com>
2025-04-23 08:55:48 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 500 ms, Memory: 57388 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,60 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static ArrayList<Integer>[] graph;
-	static int[] parent;
+	static int n;
+	static List<Integer>[] tree;
 	static boolean[] visited;
+	static int[] answer;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 
-		graph = new ArrayList[N + 1];
-		parent = new int[N + 1];
-		visited = new boolean[N + 1];
-
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
+		visited = new boolean[n + 1];
+		answer = new int[n + 1];
+		tree = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++) {
+			tree[i] = new ArrayList<>();
 		}
 
-		for (int i = 0; i < N - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			graph[a].add(b);
-			graph[b].add(a);
+		for (int i = 1; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+
+			tree[n1].add(n2);
+			tree[n2].add(n1);
 		}
 
 		bfs(1);
 
-		for (int i = 2; i <= N; i++) {
-			sb.append(parent[i]).append("\n");
+		for (int i = 2; i <= n; i++) {
+			sb.append(answer[i]).append("\n");
 		}
 
-		System.out.print(sb);
+		System.out.println(sb);
 	}
 
-	static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		visited[start] = true;
-		queue.add(start);
+	private static void bfs(int n) {
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+		queue.add(n);
+		visited[n] = true;
 
 		while (!queue.isEmpty()) {
-			int current = queue.poll();
+			int now = queue.poll();
 
-			for (int next : graph[current]) {
+			for (int next : tree[now]) {
 				if (!visited[next]) {
 					visited[next] = true;
-					parent[next] = current;
 					queue.add(next);
+					answer[next] = now;
 				}
 			}
 		}
