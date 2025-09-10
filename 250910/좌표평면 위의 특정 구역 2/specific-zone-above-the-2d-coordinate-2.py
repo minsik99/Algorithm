d94c8674b6d2b1bff526d955b8cc2ab2af8f47a3
minsik99 <164865777+minsik99@users.noreply.github.com>
2025-09-10 22:36:03 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250910/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 250910/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,17 +1,23 @@
+import sys
+
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 
-max_x = max(x)
-max_y = max(y)
+ans = sys.maxsize
 
-x.remove(max_x)
-y.remove(max_y)
-
-dist_x = max(x) - min(x)
-dist_y = max(y) - min(y)
-
-ans = dist_x * dist_y
+for i in range(n):
+    max_x = max_y = 0
+    min_x = min_y = 40001
+    for j in range(n):
+        if j == i:
+            continue
+        x, y = points[j]
+        min_x = min(min_x, x)
+        max_x = max(max_x, x)
+        min_y = min(min_y, y)
+        max_y = max(max_y, y)
+    ans = min(ans, (max_x - min_x) * (max_y - min_y))
 
 print(ans)
