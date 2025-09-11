23d0985063ec04ac2b1bd6e138c0da3733645c3e
minsik99 <164865777+minsik99@users.noreply.github.com>
2025-09-11 10:44:22 +0900

    Title: 방향에 맞춰 이동, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250911/방향에 맞춰 이동/move-in-direction.py
+++ 250911/방향에 맞춰 이동/move-in-direction.py
@@ -3,26 +3,13 @@ moves = [tuple(input().split()) for _ in range(n)]
 dir = [move[0] for move in moves]
 dist = [int(move[1]) for move in moves]
 
-dx, dy = [-1, 0, 0, 1], [0, -1, 1, 0]
+move_dirs = {'W': (-1, 0), 'S': (0, -1), 'N': (0, 1), 'E': (1, 0)}
 
-ans_x = ans_y = 0
+x = y = 0
 
 for i in range(n):
-    if dir[i] == 'W':
-        for _ in range(dist[i]):
-            ans_x += dx[0]
-            ans_y += dy[0]
-    elif dir[i] == 'S':
-        for _ in range(dist[i]):
-            ans_x += dx[1]
-            ans_y += dy[1]
-    elif dir[i] == 'N':
-        for _ in range(dist[i]):
-            ans_x += dx[2]
-            ans_y += dy[2]
-    elif dir[i] == 'E':
-        for _ in range(dist[i]):
-            ans_x += dx[3]
-            ans_y += dy[3]
+    dx, dy = move_dirs[dir[i]]
+    x += dx * dist[i]
+    y += dy * dist[i]
 
-print(ans_x, ans_y)
+print(x, y)
